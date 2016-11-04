e225be9f262e1752a52db8e6b2f521a1d767b35f
MoritzKn <moritz.kneilmann@gmx.de>
2016-11-04 23:50:36 +0100

    Add player walk animation

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -18,37 +18,45 @@ func _ready():
 
 
 func _fixed_process(delta):
-	var btn_top = Input.is_action_pressed("PlayerMoveTop")
-	var btn_right = Input.is_action_pressed("PlayerMoveRight")
-	var btn_bottom = Input.is_action_pressed("PlayerMoveBottom")
-	var btn_left = Input.is_action_pressed("PlayerMoveLeft")
+	var animation_player = get_node("PlayerAnimationPlayer")
 
-	if (btn_top):
+	var btn_top_pressed = Input.is_action_pressed("PlayerMoveTop")
+	var btn_right_pressed = Input.is_action_pressed("PlayerMoveRight")
+	var btn_bottom_pressed = Input.is_action_pressed("PlayerMoveBottom")
+	var btn_left_pressed = Input.is_action_pressed("PlayerMoveLeft")
+
+	if (btn_top_pressed):
 		move(Vector2(0, -speed))
-	if (btn_right):
+	if (btn_right_pressed):
 		move(Vector2(speed, 0))
-	if (btn_bottom):
+	if (btn_bottom_pressed):
 		move(Vector2(0, speed))
-	if (btn_left):
+	if (btn_left_pressed):
 		move(Vector2(-speed, 0))
 
 	var dir = null
-	if (btn_top && btn_right):
+	if (btn_top_pressed && btn_right_pressed):
 		dir = DIR_NE
-	elif (btn_top):
+	elif (btn_top_pressed):
 		dir = DIR_N
-	elif (btn_right && btn_bottom):
+	elif (btn_right_pressed && btn_bottom_pressed):
 		dir = DIR_SE
-	elif (btn_right):
+	elif (btn_right_pressed):
 		dir = DIR_E
-	elif (btn_bottom && btn_left):
+	elif (btn_bottom_pressed && btn_left_pressed):
 		dir = DIR_SW
-	elif (btn_bottom):
+	elif (btn_bottom_pressed):
 		dir = DIR_S
-	elif (btn_left && btn_top):
+	elif (btn_left_pressed && btn_top_pressed):
 		dir = DIR_NW
-	elif (btn_left):
+	elif (btn_left_pressed):
 		dir = DIR_W
 
+
 	if (dir != null):
-		set_rotd((dir * 45) - init_rot);
+		set_rotd((dir * 45) - init_rot)
+
+		if (!animation_player.is_playing()):
+			animation_player.play("Walk")
+	else:
+		animation_player.stop();
