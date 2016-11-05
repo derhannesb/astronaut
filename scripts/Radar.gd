8b6bbcceb9b1b7ba4b6db7fb824df76fd8121ed7
MoritzKn <moritz.kneilmann@gmx.de>
2016-11-05 22:24:20 +0100

    Turn radar 180deg

--- scripts/Radar.gd
+++ scripts/Radar.gd
@@ -5,6 +5,6 @@ func _ready():
 
 func _process(delta):
 	#set_global_rot( get_global_pos().angle_to( get_node("/root/Main/SpaceStation").get_global_pos() ))
-	
-	
-	set_global_rot ( Utils.vec_to_radians(get_node("/root/Main/SpaceStation").get_global_pos() - get_global_pos()) )
+
+
+	set_global_rot ( Utils.vec_to_radians(get_node("/root/Main/SpaceStation").get_global_pos() - get_global_pos()) + PI)
