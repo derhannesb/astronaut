54f8c9fcb26111c9fef90dbf0f4794db38b09a68
derhannesb <johannes.bade@gmail.com>
2016-11-05 21:17:56 +0100

    add radar

---
+++ scripts/Radar.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	#set_global_rot( get_global_pos().angle_to( get_node("/root/Main/SpaceStation").get_global_pos() ))
+	
+	
+	set_global_rot ( Utils.vec_to_radians(get_node("/root/Main/SpaceStation").get_global_pos() - get_global_pos()) )
--- scripts/Utils.gd
+++ scripts/Utils.gd
@@ -2,3 +2,6 @@ extends Node
 
 func radians_to_vec(radians):
 	return Vector2(sin(radians), cos(radians)).normalized()
+
+func vec_to_radians(vec):
+	return atan2(vec.x, vec.y) 
