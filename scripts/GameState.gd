34b104d5a412815b621e02cc51550a5a833a72b1
MoritzKn <moritz.kneilmann@gmx.de>
2016-11-07 17:46:03 +0100

    Stop breathing after game over

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,7 +1,7 @@
 extends Node
 
 export var oxygen = 100
-var oxygen_lost_per_leak = .15
+var oxygen_lost_per_leak = 0.20
 
 export var isGameOver = false
 export var isGameLost = false
@@ -13,8 +13,9 @@ func _process(delta):
 	var leaks_obj = get_node("/root/Main/Capsule/Leaks")
 	var leaks_objs = leaks_obj.get_children()
 	var leak_cont = leaks_objs.size()
-
-	oxygen -= delta * leak_cont * oxygen_lost_per_leak
+	
+	if (!isGameOver):
+		oxygen -= delta * leak_cont * oxygen_lost_per_leak
 
 	if (oxygen <= 0):
 		loseGame()
@@ -23,9 +24,9 @@ func _process(delta):
 		var amount = ((oxygen - 20) / 2) + 10
 		leak.get_node("Particles2D").set_amount(amount)
 		var stream_player = leak.get_node("StreamPlayer")
-		stream_player.set_volume_db(-30+(oxygen/3))
+		stream_player.set_volume_db(-30 + (oxygen / 3))
 
-		if (isGameOver):
+		if (oxygen <= 0):
 			leak.get_node("Particles2D").hide()
 			stream_player.stop()
 
@@ -37,6 +38,7 @@ func loseGame():
 	isGameOver = true
 	get_node("/root/Main/CanvasLayer/GUI/GameOver").show()
 	get_node("/root/Main/CanvasLayer/GUI/SFXGameOver").play()
+	get_node("/root/Main/Capsule/Player/Breathing").stop()
 	print("Lose")
 
 func winGame():
@@ -44,13 +46,12 @@ func winGame():
 		return
 
 	isGameOver = true
+	
 	get_node("/root/Main/CanvasLayer/GUI/Victory").show()
 	var capsule = get_node("/root/Main/Capsule")
-
 	capsule.set_linear_velocity(Vector2(0, 0))
 	capsule.set_angular_velocity(0)
 	capsule.set_angular_damp(1000)
 	capsule.set_linear_damp(1000)
-
 
 	print("Win")
