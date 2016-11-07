0ef5a7d398309fbeb1cbacb8b4da83407f831383
MoritzKn <moritz.kneilmann@gmx.de>
2016-11-07 21:03:55 +0100

    Fix distance to crane bug
    this will ensure that the crane always hits the player,
    no matter from which direction the player comes

--- scripts/Crane.gd
+++ scripts/Crane.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 var crane_target = null
 var crane_ready = false
+var animation_player
 
 func _ready():
 	crane_target = get_node("/root/Main/Capsule")
+	animation_player = get_node("AnimationPlayer")
 	set_process(true)
 
 func _process(delta):
 	var dir = crane_target.get_global_pos() - get_global_pos()
 	var dist = dir.length()
-	var animation_player = get_node("AnimationPlayer")
 
 	var target_rot = Utils.vec_to_radians(dir) - PI
 
@@ -20,7 +21,10 @@ func _process(delta):
 	if (crane_ready):
 		set_global_rot(target_rot)
 
-		if (dist < 600 && animation_player.get_current_animation() != "grab"):
+		if (dist < 900 && animation_player.get_current_animation() != "grab"):
 			animation_player.play("grab")
-	elif(!crane_ready && dist > 1000):
+			
+		if (dist < 550):
+			GameState.winGame()
+	elif(dist > 1000):
 		crane_ready = true
--- scripts/Radar.gd
+++ scripts/Radar.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+var crane
+
 func _ready():
+	crane = get_node("/root/Main/SpaceStation/Crane")
 	set_process(true)
 
 func _process(delta):
-	var station = get_node("/root/Main/SpaceStation/Sprite")
+	var station = get_node("/root/Main/SpaceStation")
 	var dir = station.get_global_pos() - get_global_pos()
 	set_global_rot(Utils.vec_to_radians(dir) + PI)
-	if (dir.length() < 680):
-		GameState.winGame()
